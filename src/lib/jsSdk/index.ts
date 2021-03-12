0bf30d1bc568484dcc67ba0c7bff60636660b485
haixiangyan <haixiang6123@gmail.com>
2021-03-12 15:07:45 +0800

    feat: 去掉冗余的接口

--- src/lib/jsSdk/index.ts
+++ src/lib/jsSdk/index.ts
@@ -1,5 +1,4 @@
 import compareVersions from 'compare-versions'
-import { Msg, MsgType } from './types';
 export { mockWxSDK } from './utils';
 
 /**
@@ -53,7 +52,7 @@ const checkDeprecated = (): boolean => {
  * @param apiName api 名称
  * @param params 传入参数
  */
-const invoke = <Res = {}>(apiName: wx.Api, params: Object) => {
+const invoke = <Res = {}>(apiName: wx.Api, params = {}) => {
   return new Promise<wx.InvokeCallbackRes & Res>((resolve, reject) => {
     wx.invoke<Res>(apiName, params, res => {
       if (res.err_msg === `${apiName}:ok`) {
@@ -65,65 +64,11 @@ const invoke = <Res = {}>(apiName: wx.Api, params: Object) => {
   });
 };
 
-/**
- * 从外部联系人的profile、聊天工具栏进入页面时才可成功调用该接口
- * 详见：https://work.weixin.qq.com/api/doc/90000/90136/91799
- */
-const getCurExternalContact = async () => {
-  interface Res {
-    userId: string;
-  }
-
-  const res = await invoke<Res>('getCurExternalContact', {});
-
-  return res.userId;
-};
-
-/**
- * 从客户群的聊天工具栏进入页面时才可成功调用该接口
- * 详见：https://work.weixin.qq.com/api/doc/90000/90136/92095
- */
-const getCurExternalChat = async () => {
-  interface Res {
-    chatId: string;
-  }
-
-  const res = await invoke<Res>('getCurExternalChat', {});
-
-  return res.chatId;
-};
-
-/**
- * 聊天工具栏分享消息到会话
- */
-const sendChatMessage = async (msgType: MsgType, value: Msg) => {
-  return invoke('sendChatMessage', {
-    msgtype: msgType,
-    [msgType]: value,
-  });
-};
-
-/**
- * 调用此接口将唤起成员或外部联系人的个人信息页面
- * @param userId
- * @param type
- */
-const openUserProfile = async (userId: string, type: 1 | 2) => {
-  return invoke('openUserProfile', {
-    type,
-    userid: userId
-  })
-}
-
 const wecomJsSdk = {
   checkDeprecated,
   config,
   agentConfig,
-  getCurExternalContact,
-  getCurExternalChat,
-  sendChatMessage,
   invoke,
-  openUserProfile,
 };
 
 export default wecomJsSdk;
