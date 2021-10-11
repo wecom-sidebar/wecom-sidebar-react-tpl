03c87eb0ba9559fc072d247fae1fd0bef19501f1
haixiangyan <haixiang6123@gmail.com>
2021-10-11 23:41:12 +0800

    feat: 收敛 createJsSdk

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,16 +1,6 @@
-import _jsSdk from "./jsSdk"
-import {isMock, mockJsSdk} from "./jsSdk/utils"
 import checkRedirect from "./utils/checkRedirect";
 import initSdk from "./utils/initSdk";
-
-/**
- * 生成 jssdk 的函数
- * @param wxResMock wx.fn 的 mock 映射关系
- * @param invokeResMock wx.invoke 的 mock 映射关系
- */
-const createJsSdk = (wxResMock?: Record<string, any>, invokeResMock?: Record<string, any>) => {
-  return isMock ? mockJsSdk(_jsSdk, wxResMock || {}, invokeResMock || {}) : _jsSdk;
-}
+import createJsSdk from "./utils/createJsSdk";
 
 export {
   createJsSdk,
