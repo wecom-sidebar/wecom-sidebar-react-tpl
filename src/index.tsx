47072bbfcb0d3f1df56f2bdfed652b313cb7c62b
haixiangyan <haixiang6123@gmail.com>
2021-10-15 07:44:46 +0800

    feat: 添加 antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ConfigProvider} from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
 import App from './App'
 import {fetchUserId, fetchSignatures} from './api'
 import config from './_config'
 import {invokeResMock, wxResMock} from "./mock";
 import {checkRedirect, createJsSdk, initSdk} from "./lib";
 
+import 'antd/dist/antd.css';
+
 export const jsSdk = createJsSdk(wxResMock, invokeResMock);
+
+const AppWrapper = (
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>
+)
 
 checkRedirect(config, fetchUserId)
   .then(() => initSdk(config, fetchSignatures))
-  .then(() => ReactDOM.render(<App/>, document.getElementById('root')))
+  .then(() => ReactDOM.render(AppWrapper, document.getElementById('root')))
