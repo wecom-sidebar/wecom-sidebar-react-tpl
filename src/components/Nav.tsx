85090c809d25898dd968a8d7e505825f1d3f7ed3
haixiangyan <haixiang6123@gmail.com>
2021-10-15 08:30:20 +0800

    feat: 更换图标

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from "react";
 import {Menu} from "antd";
-import {HomeOutlined, SettingOutlined} from "@ant-design/icons";
+import {HomeOutlined, SettingOutlined, UsergroupAddOutlined, UserOutlined} from "@ant-design/icons";
 import {Link} from "react-router-dom";
 
 const Nav: FC = () => {
@@ -12,10 +12,10 @@ const Nav: FC = () => {
       <Menu.Item key="actions" icon={<SettingOutlined/>}>
         <Link to="/actions">操作</Link>
       </Menu.Item>
-      <Menu.Item key="externalUser" icon={<SettingOutlined/>}>
+      <Menu.Item key="externalUser" icon={<UserOutlined />}>
         <Link to="/external-user">外部联系人</Link>
       </Menu.Item>
-      <Menu.Item key="externalChat" icon={<SettingOutlined/>}>
+      <Menu.Item key="externalChat" icon={<UsergroupAddOutlined />}>
         <Link to="/external-chat">外部联系群</Link>
       </Menu.Item>
     </Menu>
